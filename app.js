638bd76446813e3671b78664fbec23b1f2a6fd22
Shaylene Sunday <allkatt@yahoo.com>
2024-10-15 11:56:30 -0600

    fixed the graph and social stats

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function timeFrame(period){
         trafficChart.data.datasets[0].data = monthly;
     }
     trafficChart.update();
-}
+};
 
 
 
@@ -173,13 +173,12 @@ let mobileChart = new Chart(mobileCanvas, {
 const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
-
-user.addEventListener('click', () => {
-    if (user.value === "" && message.value === " "){
+send.addEventListener('click', () => {
+    if (user.value === "" && message.value === ""){
         alert("Please fill out user and message fields before sending");
-    } else if (user.value === " ") {
+    } else if (user.value === "") {
         alert("Please make sure you fill in the user field");
-    } else if (message.value === " "){
+    } else if (message.value === ""){
         alert("Please fill in the message field before sending");
     } else {
         alert(`Message successfully sent to: ${user.value}`);
@@ -238,8 +237,25 @@ cancel.addEventListener('click', (e) => {
     timezoneToggle.value = 'Default';
 });
 
-// traffic line graphs 
 
-document.getElementById('hourly').onclick = function() {
-    timeFrame();
-};
+
+
+const trafficNav = document.querySelector('.traffic-nav');
+
+trafficNav.addEventListener('click', (e) => {
+  if (e.target.classList.contains('hourly')) {
+    timeFrame('hourly');
+  }
+
+  if (e.target.classList.contains('daily')) {
+    timeFrame('daily');
+  }
+
+  if (e.target.classList.contains('weekly')) {
+    timeFrame('weekly');
+  }
+
+  if (e.target.classList.contains('monthly')) {
+    timeFrame('monthly');
+  }
+});
